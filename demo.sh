75c10b3c6a9fe10c5f8380064afb3c087517ec2d
sudhakard-cloudside <87324686+sudhakar678@users.noreply.github.com>
2022-11-30 15:16:24 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -3,19 +3,7 @@
 gcloud container images describe us-central1-docker.pkg.dev/cloudside-academy/sudhakar-test/test-image:$_TAG --format 'value(image_summary.digest)' > sha
 
 ls
+
 cat sha
 
-export DIGEST=$(awk 'NR==1{print $1}' sha)
 
-gcloud container binauthz create-signature-payload \
---artifact-url=us-central1-docker.pkg.dev/cloudside-academy/sudhakar-test/test-image@$DIGEST > ./generated_payload.json
-
-openssl dgst -sha256 -sign ./ec_private.pem ./generated_payload.json > ./ec_signature
-
-gcloud container binauthz attestations create \
-    --project="${PROJECT_ID}" \
-    --artifact-url="${IMAGE_TO_ATTEST}" \
-    --attestor="projects/${PROJECT_ID}/attestors/${ATTESTOR_NAME}" \
-    --signature-file=/tmp/ec_signature \
-    --public-key-id="ec_public.pem" \
-    --validate
