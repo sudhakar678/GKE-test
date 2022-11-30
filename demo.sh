5fc0cf775329d9f3f61c18db8b6acb1e7bbbed3a
sudhakard-cloudside <87324686+sudhakar678@users.noreply.github.com>
2022-11-30 12:40:29 +0530

    Create demo.sh

---
+++ demo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+  
+IMAGE_PATH="us-central1-docker.pkg.dev/cloudside-academy/sudhakar-test/test-image:$BUILD_ID"
+IMAGE_TO_ATTEST=${IMAGE_PATH}
+
+gcloud container binauthz create-signature-payload \
+--artifact-url=${IMAGE_TO_ATTEST} > ./generated_payload.json
+
+openssl dgst -sha256 -sign ./ec_private.pem ./generated_payload.json > ./ec_signature
+
+gcloud container binauthz attestations create \
+    --project="${PROJECT_ID}" \
+    --artifact-url="${IMAGE_TO_ATTEST}" \
+    --attestor="projects/${PROJECT_ID}/attestors/${ATTESTOR_NAME}" \
+    --signature-file=/tmp/ec_signature \
+    --public-key-id="ec_public.pem" \
+    --validate
