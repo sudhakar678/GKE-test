0b53e26708c09e46bf31e9d42e3bfa8337dfa00e
sudhakard-cloudside <87324686+sudhakar678@users.noreply.github.com>
2022-11-30 15:04:16 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
   
-IMAGE_PATH="us-central1-docker.pkg.dev/cloudside-academy/sudhakar-test/test-image:$GITHUB_SHA"
-IMAGE_TO_ATTEST=${IMAGE_PATH}
+export DIGEST= (gcloud container images describe  us-central1-docker.pkg.dev/cloudside-academy/sudhakar-test/test-image:$_TAG  --format 'value(image_summary.digest)')
+echo $DIGEST
 
 gcloud container binauthz create-signature-payload \
---artifact-url=${IMAGE_TO_ATTEST} > ./generated_payload.json
+--artifact-url=us-central1-docker.pkg.dev/cloudside-academy/sudhakar-test/test-image@$DIGEST > ./generated_payload.json
 
 openssl dgst -sha256 -sign ./ec_private.pem ./generated_payload.json > ./ec_signature
 
