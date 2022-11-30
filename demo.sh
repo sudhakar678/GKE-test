1ac3fe67af9a8c6341204274567b58d3c101ebc5
sudhakard-cloudside <87324686+sudhakar678@users.noreply.github.com>
2022-11-30 19:28:31 +0530

    Delete demo.sh

--- demo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-  
-gcloud container images describe us-central1-docker.pkg.dev/cloudside-academy/sudhakar-test/test-image:$_TAG --format 'value(image_summary.digest)' > sha
-
-ls
-
-cat index.html
-
-
